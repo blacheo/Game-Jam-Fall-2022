eb96857aa1262a298af93137f3790a064d3f9ab1
ericnem <ericnem@outlook.com>
2022-10-01 15:20:24 -0400

    Ground Collision
    Goose falls on ground and collides.

--- Player.gd
+++ Player.gd
@@ -1,35 +1,25 @@
-extends Area2D
+extends KinematicBody2D
 
-export var speed = 400
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var run_speed = 350
+var jump_speed = -1000
+var gravity = 10
 
+var velocity = Vector2()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_just_pressed('ui_select')
 
+	if is_on_floor() and jump:
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+	if left:
+		velocity.x -= run_speed
 
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-		$AnimatedSprite.flip_h = true
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-
-	if position.y < 100: # this will need to be the platform height instead of a fixed number.
-		velocity.y += 0.2
-	
-
-	if abs(velocity.x) > 0 or abs(velocity.y) > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.animation = "running"
-	else:
-		$AnimatedSprite.animation = "idle"
-		
-	position += velocity * delta
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
