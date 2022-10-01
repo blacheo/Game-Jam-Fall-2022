0cdd5a78a1f92716ca172c1930f0ece5c2f1e00c
ericnem <ericnem@outlook.com>
2022-10-01 16:57:29 -0400

    Health Bar Added
    Shows hearts at top left corner.

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ const ZERO_VELOCITY_Y = 6.7
 const RUN_SPEED = 350
 const JUMP_SPEED = 400 # Pixels up
 const GRAVITY = 400
+var health = 3
 
 var velocity = Vector2()
 
@@ -13,7 +14,8 @@ func get_input():
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_up')
-
+	
+	# $Heart1.visible = not $Heart1.visible
 	if is_on_floor() and jump:
 		velocity.y -= JUMP_SPEED
 	
@@ -24,7 +26,7 @@ func get_input():
 		velocity.x -= RUN_SPEED
 		$AnimatedSprite.flip_h = true
 	
-	print(velocity.y)
+	updateHearts()
 	if abs(velocity.x) > 0 or abs(velocity.y) > ZERO_VELOCITY_Y:
 		$AnimatedSprite.animation = "running"
 	else:
@@ -35,3 +37,18 @@ func _physics_process(delta):
 	velocity.y += GRAVITY * delta
 	get_input()
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+func updateHearts():
+	if health == 3:
+		$Heart1.visible = true
+		$Heart2.visible = true
+		$Heart3.visible = true
+	elif health == 2:
+		$Heart1.visible = true
+		$Heart2.visible = true
+		$Heart3.visible = false
+	elif health == 1:
+		$Heart1.visible = true
+		$Heart2.visible = false
+		$Heart3.visible = false
+		
