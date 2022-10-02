4065ba2216594cc51fe21dd96fda08f9eb511a9a
blaicheo <78671393+blacheo@users.noreply.github.com>
2022-10-02 11:17:44 -0400

    Play Button Functionality

--- UI/Main Menu.gd
+++ UI/Main Menu.gd
@@ -1,14 +1,29 @@
 extends Node
 
+var current_scene = null
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var root = get_tree().get_root()
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+	
+
+func goto_scene(path):
+	call_deferred("_deferred_goto_scene", path)
+	
+func _deferred_goto_scene(path):
+	current_scene.queue_free()
+	
+	var s = ResourceLoader.load(path)
+	
+	current_scene = s.instance()
+	get_tree().get_root().add_child(current_scene)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +33,6 @@ func _ready():
 
 func _on_Exit_pressed():
 	get_tree().quit()
+	
+func _on_Play_pressed():
+	goto_scene("res://Main.tscn")
